1e6bf484df37bfe299ed2ae8f44d85ac3e279f46
lucashthiele <lucasthiele0@outlook.com>
2021-09-17 20:49:21 -0300

    global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363F5F;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF;
+  }
+  
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;// 15 px
+    }
+
+    @media (max-width:720px){
+      font-size: 87.5%;// 14 px
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6s;
+    cursor: not-allowed;
+  }
+`;
