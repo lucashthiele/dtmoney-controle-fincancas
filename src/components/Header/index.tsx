661acc0ad0901c5123ca0ff203f77d512d095f0b
lucashthiele <lucasthiele0@outlook.com>
2021-09-19 00:25:37 -0300

    modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import logo from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header(props: HeaderProps) {
   return (
     <>
       <Container>
         <Content>
           <img src={logo} alt="dt money" />
-          <button type="button">Nova Transação</button>
+          <button type="button" onClick={props.onOpenNewTransactionModal}>
+            Nova Transação
+          </button>
         </Content>
       </Container>
     </>
