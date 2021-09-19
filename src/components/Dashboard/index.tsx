f19041d10744d2179e4473a16d9ce1b036c3fbcc
lucashthiele <lucasthiele0@outlook.com>
2021-09-19 19:00:28 -0300

    submit to api

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,26 +1,6 @@
 import { Summary } from "../Summary";
 import { TransactionTable } from "../TransactionsTable";
 import { Container } from "./styles";
-import { createServer } from "miragejs";
-
-createServer({
-  routes() {
-    this.namespace = "api";
-
-    this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction",
-          amount: 4000,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
-        },
-      ];
-    });
-  },
-});
 
 export function Dashboard() {
   return (
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import Modal from "react-modal";
 import income from "../../assets/income.svg";
 import outcome from "../../assets/outcome.svg";
 import close from "../../assets/close.svg";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,7 +12,23 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal(props: NewTransactionModalProps) {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+
+    api.post("/transactions", data);
+  }
 
   return (
     <>
@@ -29,11 +46,20 @@ export function NewTransactionModal(props: NewTransactionModalProps) {
           <img src={close} alt="Fechar Modal" />
         </button>
 
-        <Container>
+        <Container onSubmit={(event) => handleCreateNewTransaction(event)}>
           <h2>Cadastrar Transação</h2>
 
-          <input placeholder="Título" />
-          <input placeholder="Valor" type="number" />
+          <input
+            placeholder="Título"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <input
+            placeholder="Valor"
+            type="number"
+            value={value}
+            onChange={(event) => setValue(Number(event.target.value))}
+          />
 
           <TrasactionTypeContainer>
             <RadioBox
@@ -61,7 +87,11 @@ export function NewTransactionModal(props: NewTransactionModalProps) {
             </RadioBox>
           </TrasactionTypeContainer>
 
-          <input placeholder="Categoria" />
+          <input
+            placeholder="Categoria"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          />
           <button type="submit">Cadastar</button>
         </Container>
       </Modal>
